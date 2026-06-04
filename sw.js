b673c4036837b1481dd6e185f3ba2497a8e434a6
David <davidmprimo@gmail.com>
2026-06-04 20:08:33 +0200

    TASF y SW.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-/* HT Iryo — Service Worker */
-const CACHE_NAME = 'ebula-v1';
+
+const CACHE_NAME = 'ebula-v2';
 const PRECACHE = [
   './',
   './index.html',
+  './boxann.js',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png',
